d6c89b56478ebd8b84acc9f11e1a2f2b6b5beacb
E-commerce <shubhamramane143@gmail.com>
2026-02-21 20:27:15 +0530

    improve about page

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,7 +1,11 @@
 const AboutPage = () => {
-    return ( <>
-    <h1 className="text-3xl font-bold text-white mb-2">This is about page</h1>
-    </> );
-}
- 
+	return (
+		<>
+			<h1 className="text-3xl font-bold text-white mb-2">
+				This is about page
+			</h1>
+		</>
+	);
+};
+
 export default AboutPage;

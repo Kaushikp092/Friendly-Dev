31ff2e100bf068be6f1f91bc3d86cfde08753910
E-commerce <shubhamramane143@gmail.com>
2026-02-21 20:26:59 +0530

    remove section tag and added fragment

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,12 +1,13 @@
 const ContactPage = () => {
-    return ( <>
-    <section>
-        <h2 className="text-3xl font-bold text-white mb-8 text-center">
-            ☎️Contact me
-        </h2>
-    </section>
-    </>
-     );
-}
- 
+	return (
+		<>
+			<>
+				<h2 className="text-3xl font-bold text-white mb-8 text-center">
+					☎️Contact me
+				</h2>
+			</>
+		</>
+	);
+};
+
 export default ContactPage;

cf9519051deca6ebb1c3d3ba01e14affc24ac179
kaushikp092 <kaushikpatil092@gmail.com>
2026-03-06 15:04:28 +0530

    Display loader data

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
+import ProjectCard from "~/components/ProjectCard"
 
-export async function loader({ request }: Route.LoaderArgs): Promise<{projects: Project[]}> {
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
 	const res = await fetch("http://localhost:8000/projects");
 	const data = await res.json();
 
@@ -9,12 +12,18 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{projects: 
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-	const { projects } = loaderData as {projects: Project[]};
-	console.log(projects);
+	const { projects } = loaderData as { projects: Project[] };
+	// console.log(projects);
 
 	return (
 		<>
 			<h2 className="text-3xl text-white font-bold mb-8">🚀Projects</h2>
+
+			<div className="grid gap-6 sm:grid-cols-2">
+				{projects.map((project) => (
+					<ProjectCard key={project.id} project={project}/>
+				))}
+			</div>
 		</>
 	);
 };
